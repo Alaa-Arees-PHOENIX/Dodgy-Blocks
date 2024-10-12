47a15cbade2a2345f73f403281fc1e8847d00e96
Alaa Arees ~ PHOENIX <alaaareesgithub@gmail.com>
2024-10-12 17:17:45 +0300

    Putting final touches, added a version to the game.

--- src/AboutMenu.hpp
+++ src/AboutMenu.hpp
@@ -9,16 +9,16 @@ private:
 	/* A well formatted version of the description message:
 	static constexpr char msg1[] = "Made by Alaa Arees (PHOENIX) using Raylib library.\n
 									All music and sound contents are made by Ali Ibrahim (Beyond Electric).\n
-									Please read the 'Readme.md' file accompanied in the game folder for more\n
+									Please read the 'README.md' file accompanied in the game folder for more\n
 									details about the game, authors and everything else.
-									Current version: unknown\n
+									Current version: 3.0.0\n
 									this game is open source and written in C++, if you want\n
 									to check the source code or if you have any feedbacks\n
 									feel free to contact us, contact info are also available\n
-									in the 'Readme.md' file.";
+									in the 'README.md' file.";
 	*/
 	/* A fucked up version of the same message because Mr. C++ does not support declaring the string on multiple lines: */
-	static constexpr char msg1[] = "Made by Alaa Arees (PHOENIX) using Raylib library. \nAll music and sound contents are made by Ali Ibrahim (Beyond Electric). \nPlease read the 'Readme.md' file accompanied in the game folder for \nmore details about the game, authors and everything else. \nCurrent version: unknown \nthis game is open source and written in C++, if you want \nto check the source code or if you have any feedbacks \nfeel free to contact us, contact info are also available \nin the 'Readme.md' file.";
+	static constexpr char msg1[] = "Made by Alaa Arees (PHOENIX) using Raylib library. \nAll music and sound contents are made by Ali Ibrahim (Beyond Electric). \nPlease read the 'README.md' file accompanied in the game folder for \nmore details about the game, authors and everything else. \nCurrent version: 3.0.0 \nthis game is open source and written in C++, if you want \nto check the source code or if you have any feedbacks \nfeel free to contact us, contact info are also available \nin the 'README.md' file.";
 	
 public:
 	AboutMenu ();
